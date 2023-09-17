fb060a36cacec385b83b681047ff96c9b59e58ae
jorge <jorivalope@gmail.com>
2023-09-17 07:40:05 -0500

    Segundo commit

---
+++ src/main/java/com/ps/jac16/Jac16Application.java
@@ -0,0 +1,13 @@
+package com.ps.jac16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jac16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jac16Application.class, args);
+	}
+
+}
